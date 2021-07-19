108012a0ab5485b6126103764e07b5f199bb1c4c
Clinger <clinger.de@gmail.com>
2021-07-18 23:53:15 -0300

    update
    Criação das páginas de depoimentos e contatos

--- main.js
+++ main.js
@@ -17,3 +17,43 @@ for(const link of links){
         nav.classList.remove('show')
     })
 }
+
+/* mudar o header da página quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY >= navHeight){
+        header.classList.add('scroll')
+    }else{
+        header.classList.remove('scroll')
+    }
+})
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página*/
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `,
+    {interval: 100}
+)
